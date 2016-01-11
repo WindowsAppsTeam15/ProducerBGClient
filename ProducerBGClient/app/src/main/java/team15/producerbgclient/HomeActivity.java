07b227f9ac01c53902659f7892f1a4abeb5e09ae
veselints <veselintsvetanov@gmail.com>
2016-01-12 00:32:58 +0200

    Added basic menu

--- ProducerBGClient/app/src/main/java/team15/producerbgclient/HomeActivity.java
+++ ProducerBGClient/app/src/main/java/team15/producerbgclient/HomeActivity.java
@@ -3,8 +3,13 @@ package team15.producerbgclient;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.*;
+import android.graphics.*;
+import android.content.*;
+import android.view.*;
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -13,10 +18,18 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+
+
         Button goToRegisterButton = (Button) findViewById(R.id.btn_go_to_register);
         goToRegisterButton.setOnClickListener(this);
         Button goToAllProducersButton = (Button) findViewById(R.id.btn_all_producers);
         goToAllProducersButton.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
     @Override
