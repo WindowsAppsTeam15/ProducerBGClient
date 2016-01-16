7af4f0a726077189c1e53fd4a17081b8093a89e6
veselints <veselintsvetanov@gmail.com>
2016-01-16 17:42:38 +0200

    Search implemented

--- ProducerBGClient/app/src/main/java/team15/producerbgclient/BaseActivity.java
+++ ProducerBGClient/app/src/main/java/team15/producerbgclient/BaseActivity.java
@@ -3,10 +3,16 @@ package team15.producerbgclient;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -14,6 +20,8 @@ import android.widget.Toast;
  * Created by veso on 1/12/2016.
  */
 public abstract class BaseActivity extends AppCompatActivity {
+    SearchView searchView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,9 +31,38 @@ public abstract class BaseActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
 
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
         MenuItem manageProducersBtn = (MenuItem) menu.findItem(R.id.action_manage);
         MenuItem addProducersBtn = (MenuItem) menu.findItem(R.id.action_add);
         MenuItem logoutBtn = (MenuItem) menu.findItem(R.id.action_logout);
+
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                ConnectivityManager connMgr = (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+                if (networkInfo == null || !networkInfo.isConnected()) {
+                    Toast.makeText(getApplicationContext(), "No internet connection!", Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+
+                Intent intent = new Intent(getApplication(), ProducersActivity.class);
+                if (query != ""){
+                    intent.putExtra("Query", query);
+                }
+                startActivity(intent);
+
+                Toast.makeText(getApplicationContext(), query, Toast.LENGTH_SHORT).show();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
 
         manageProducersBtn.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
             @Override
@@ -61,14 +98,6 @@ public abstract class BaseActivity extends AppCompatActivity {
             }
         });
 
-//        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-//        MenuItem searchItem = menu.findItem(R.id.search_menu_item);
-//        SearchView searchView = (SearchView) searchItem.getActionView();
-//        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-
-
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
-
-
 }
